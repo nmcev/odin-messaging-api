44b80358234c317c6e145d7f1317f178ea90807a
nmcev <1student.muhammed1@gmail.com>
2024-06-24 03:27:16 +0300

    refactor: Add a front end link to allowed origins in app.js and socketHandler.js

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(morgan("tiny"));
 
 // cors setup
 app.use(cors({
-    origin: ['http://localhost:5173'],
+    origin: ['http://localhost:5173', 'https://talkmate.muha.tech'],
     credentials: true
 }))
 // mongo connection 
