09f68b852077764e5668865dde0c4e29650ca06f
nmcev <1student.muhammed1@gmail.com>
2024-06-25 16:16:51 +0300

    feat: Add route to get all global messages

--- routes/message.js
+++ routes/message.js
@@ -6,4 +6,7 @@ const router = express.Router();
 // get messages of a chat
 router.get('/messages/:otherUserId', authenticateToken, MessagesController.messages_get )
 
+// get all global messages
+router.get('/global-messages', authenticateToken, MessagesController.globalMessages_get );
+
 module.exports = router;
