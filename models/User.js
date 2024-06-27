bdcadf88add7dbf1cfc9cd8eabae6dd951a3ae3c
nmcev <1student.muhammed1@gmail.com>
2024-06-28 00:46:51 +0300

    refactor: make username field unique in User model

--- models/User.js
+++ models/User.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema
 
 
 const userSchema = new Schema({
-    username: {type: String, require: true},
+    username: {type: String, require: true, unique: true},
     password: {type: String, require: true},
     profilePic: {type: String, default: 'https://odin-blog-bucket.s3.eu-north-1.amazonaws.com/3683ee82d45b76e04341886e87616e8c'},
     joinedAt: {type: String, default: Date.now()},
