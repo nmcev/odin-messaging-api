c41bd7fd78ec36e036a016924072d42fc9868382
muha <1student.muhammed1@gmail.com>
2026-09-17 22:57:59 +0300

    fix: joinedAt date runs when new a user created

--- models/User.js
+++ models/User.js
@@ -6,7 +6,7 @@ const userSchema = new Schema({
     username: {type: String, require: true, unique: true},
     password: {type: String, require: true},
     profilePic: {type: String, default: 'https://odin-blog-bucket.s3.eu-north-1.amazonaws.com/3683ee82d45b76e04341886e87616e8c'},
-    joinedAt: {type: String, default: Date.now()},
+    joinedAt: {type: String, default: Date.now},
 })
 
 module.exports = mongoose.model('User', userSchema)
